d1cdebed2776b56935e3d977f97efbb478ca7c2b
Gerax Sotelo <gsotelo23@gmail.com>
2020-01-23 16:46:35 -0700

    Change some words around

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace rock_paper_scissors
 {
@@ -19,19 +20,19 @@ namespace rock_paper_scissors
         {
           if (compChoice == 0)
           {
-            Console.WriteLine("You chose Rock. AI chose Rock");
+            Console.WriteLine("You chose Rock. AI chose Rock.");
             Console.WriteLine("DRAW!");
           }
           else if (compChoice == 1)
           {
-            Console.WriteLine("You chose Rock. AI chose Paper");
+            Console.WriteLine("You chose Rock. AI chose Paper.");
             Console.WriteLine("YOU LOSE!");
 
           }
           else if (compChoice == 2)
           {
-            Console.WriteLine("You chose Rock. AI chose Scissors");
-            Console.WriteLine("YOU WIN!");
+            Console.WriteLine("You chose Rock. AI chose Scissors.");
+            Console.WriteLine("YOU WIN!!!");
           }
           validate = false;
         }
@@ -39,18 +40,18 @@ namespace rock_paper_scissors
         {
           if (compChoice == 0)
           {
-            Console.WriteLine("You chose Paper. AI chose Rock");
-            Console.WriteLine("YOU WIN");
+            Console.WriteLine("You chose Paper. AI chose Rock.");
+            Console.WriteLine("YOU WIN!!!");
           }
           else if (compChoice == 1)
           {
-            Console.WriteLine("You chose Paper. AI chose Paper");
+            Console.WriteLine("You chose Paper. AI chose Paper.");
             Console.WriteLine("DRAW!");
 
           }
           else if (compChoice == 2)
           {
-            Console.WriteLine("You chose Paper. AI chose Scissors");
+            Console.WriteLine("You chose Paper. AI chose Scissors.");
             Console.WriteLine("YOU LOSE!");
           }
           validate = false;
@@ -59,27 +60,30 @@ namespace rock_paper_scissors
         {
           if (compChoice == 0)
           {
-            Console.WriteLine("You chose Scissors. AI chose Rock");
+            Console.WriteLine("You chose Scissors. AI chose Rock.");
             Console.WriteLine("YOU LOSE!");
           }
           else if (compChoice == 1)
           {
-            Console.WriteLine("You chose Scissors. AI chose Paper");
-            Console.WriteLine("YOU WIN!");
+            Console.WriteLine("You chose Scissors. AI chose Paper.");
+            Console.WriteLine("YOU WIN!!!");
 
           }
           else if (compChoice == 2)
           {
-            Console.WriteLine("You chose Scissors. AI chose Scissors");
+            Console.WriteLine("You chose Scissors. AI chose Scissors.");
             Console.WriteLine("DRAW!");
           }
           validate = false;
         }
         else
         {
-          Console.WriteLine("Try Again");
+          Console.WriteLine("You chose \"" + userChoice + "\". Why??");
+          Console.WriteLine("Choose Again!");
         }
       }
+
+      //Dictionary<int, string> choices = new Dictionary<int, string>();
 
       //END
     }
